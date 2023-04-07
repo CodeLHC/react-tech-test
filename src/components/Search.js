1918198eff237f55ea7dbfe110d32f8cd40d0797
CodeLHC <lhcrawford97@gmail.com>
2023-04-07 14:59:54 +0100

    app and search components,styling,tests

---
+++ src/components/Search.js
@@ -0,0 +1,10 @@
+import {React, useState} from "react";
+import "../styles/search.css"
+
+function Search() {
+  return (
+   <> <input className="search-input" type="text"/></>
+  )
+}
+
+export default Search
